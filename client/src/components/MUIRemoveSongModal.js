13aadd05b82f82cbebe63b3a81d29ea9475bf522
ebreen1 <eoin.breen@stonybrook.edu>
2022-11-07 18:31:34 -0500

    Fixed edit and remove song modals

--- client/src/components/MUIRemoveSongModal.js
+++ client/src/components/MUIRemoveSongModal.js
@@ -27,7 +27,7 @@ export default function MUIRemoveSongModal() {
         store.hideModals();
     }
     
-    let modalClass = "modal";
+    let modalClass = "modal-dialog";
     if (store.isRemoveSongModalOpen()) {
         modalClass += " is-visible";
     }
@@ -38,7 +38,7 @@ export default function MUIRemoveSongModal() {
 
     return (
         <Modal
-            open={store.listMarkedForDeletion !== null}
+            open={store.deletingSong}
         >
             <Box sx={style}>
             <div
